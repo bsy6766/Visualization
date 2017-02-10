362be0058e4ad8c660fa7a66f166726448aa7322
bsy6766 <bsy6766@gmail.com>
2017-02-10 04:22:51 -0600

    Removing VS2015 memory leak codes

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -7,15 +7,10 @@
 #include "AStarScene.h"
 #include "Utility.h"
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
 USING_NS_CC;
 
 MainScene* MainScene::createScene()
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	MainScene* newMainScene = MainScene::create();
 	return newMainScene;
 }
@@ -278,6 +273,4 @@ void MainScene::onExit()
 {
 	cocos2d::Scene::onExit();
 	releaseInputListeners();
-
-	_CrtDumpMemoryLeaks();
 }
